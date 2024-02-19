d2d5d19358e5b6b046994fac25e5217d1bd1e2bf
Nathan Yao <nathandyao@gmail.com>
2024-02-18 16:10:20 -0800

    at least we can survice when I one of the data destination is down

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def send_data(value):
+    simple_error_count = 0
     url = os.getenv("DATA_URL")
     api_url = os.getenv("API_SERVER")
     headers = {"Content-Type": "application/json", 
@@ -17,6 +18,8 @@ def send_data(value):
                 "X-AIO-Key":os.getenv("AIO_KEY"), 
                 "x-api-key":os.getenv("API_KEY")}
    
+
+
 
     # pst_tz = pytz.timezone('US/Pacific')
     # now = datetime.now()
@@ -30,24 +33,39 @@ def send_data(value):
     fmt = '%Y-%m-%d %H:%M:%S'
     now_str = now.strftime(fmt)
 
-    # data = {"value":value, "created_at":now_str}
-    # response = requests.post(url, headers=headers, json=data)
-    # print("JSON Response ", response.json())
-    
-    data = {
-                    "sgp40":{
-                        "device_id": "dashpi00",
-                        "sample": value,
-                        "sample_time" : now_str
-                        }
-                }
-    print(f"sending request to f{api_url}")
-    print(f"data {data}")
-    api_resp = requests.post(api_url, headers=headers, json=data)
-    print(f"API response {api_resp}")
-    print(f"API Resp {api_resp.content}")
-    print("api_server JSON Response ", api_resp.json())
+    try:
+        data = {"value":value, "created_at":now_str}
+        response = requests.post(url, headers=headers, json=data)
+        print("JSON Response ", response.json(url))
 
+    except Exception as e:
+        print(f"We got error post the data to {url}")
+        print(f"Error {e}")
+        simple_error_count += 1
+    
+    try:
+        data = {
+                        "sgp40":{
+                            "device_id": "dashpi00",
+                            "sample": value,
+                            "sample_time" : now_str
+                            }
+                    }
+        print(f"sending request to {api_url}")
+        print(f"data {data}")
+        api_resp = requests.post(api_url, headers=headers, json=data)
+        print(f"API response {api_resp}")
+        print(f"API Resp {api_resp.content}")
+        print("api_server JSON Response ", api_resp.json())
+
+    except Exception as e:
+        print(f"We got error post the data to {api_url}")
+        print(f"Error {e}")
+        simple_error_count += 1    
+
+    if simple_error_count >= 2:
+        #You can not have two date sinks all down
+        raise Exception("Both data destinations are down. We stop!")
 
 def run_example():
 
